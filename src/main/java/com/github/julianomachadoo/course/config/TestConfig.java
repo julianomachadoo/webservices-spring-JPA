c351b167d882301de3f9a47c250cbc0ef4af6351
Juliano Machado <julmachadoramos@gmail.com>
2022-08-05 13:20:46 -0300

    Payment, one-to-one association

--- src/main/java/com/github/julianomachadoo/course/config/TestConfig.java
+++ src/main/java/com/github/julianomachadoo/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.github.julianomachadoo.course.entities.Category;
 import com.github.julianomachadoo.course.entities.Order;
 import com.github.julianomachadoo.course.entities.OrderItem;
+import com.github.julianomachadoo.course.entities.Payment;
 import com.github.julianomachadoo.course.entities.Product;
 import com.github.julianomachadoo.course.entities.User;
 import com.github.julianomachadoo.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
