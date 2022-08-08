3c685a12d8fa479371b16fb6252a5509fcf5e3c0
Juliano Machado <julmachadoramos@gmail.com>
2022-08-08 10:16:04 -0300

    User insert

--- src/main/java/com/github/julianomachadoo/course/resources/UserResource.java
+++ src/main/java/com/github/julianomachadoo/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.github.julianomachadoo.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.julianomachadoo.course.entities.User;
 import com.github.julianomachadoo.course.services.UserService;
@@ -29,5 +33,12 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<User> insertUser(@RequestBody User obj) {
+		obj = service.insertUser(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
--- src/main/java/com/github/julianomachadoo/course/services/UserService.java
+++ src/main/java/com/github/julianomachadoo/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj  = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insertUser(User obj) {
+		return repository.save(obj);
+	}
 }
